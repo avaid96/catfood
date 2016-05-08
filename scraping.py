2b15e8cc786a95b2f5b56ef25f1a3f29616071b1
avaid96 <avaid1996@gmail.com>
2016-05-08 13:53:00 -0500

    to implement the to json

--- scraping.py
+++ scraping.py
@@ -33,22 +33,64 @@ def parseresponse(resp):
 	# #removed line breaks
 	return hours
 
-# def fromnaturallang(resp):
+def hrstosecs(hrs):
+	hour=hrs.split(':')[0]
+	mins=hrs.split(':')[1].split(' ')[0]
+	amorpm=hrs.split(' ')[1].split('.')[0]
+	secs=int(hour)*3600+int(mins)*60
+	if amorpm=='p':
+		secs+=43200
+	return int(secs)
 
-hallnames=["1835-hinman", "allison", "foster-walker", "elder", "sargent", "sargent", "willard"]
-cafenames=["einsteins", "frans", "lisas", "express"]
+def gethallrawtime():
+	"""returns a list of lists of all hall times where a hall time is in format:
+	[hallname,1setofdays,2setofdays(optional),...] where set of days is a list in format:
+	[[startday,endday(optional)],[starttime(secs),endtime(secs)]]"""
+	hallnames=["1835-hinman", "allison", "foster-walker", "elder", "sargent", "sargent", "willard"]
+	cafenames=["einsteins", "frans", "lisas", "express"]
+	daydict={'Monday':1,'Tuesday':2,'Wednesday':3,'Thursday':4,'Friday':5,'Saturday':6,'Sunday':0}
+	allhalls=[]
+	for hn in hallnames:
+		halldeets=[]
+		resp=str(scrapeapage(True, hn))
+		print hn
+		halldeets.append(hn)
+		presp=parseresponse(resp) #List of sentences like: ['Monday - Friday: 11:00 a.m. - 7:00 p.m.', 'Saturday - Sunday: Closed']
+		for s in presp:
+			sentence=[]
+			dayreg=re.compile(r'[A-Z,a-z]{3,5}day')
+			days=dayreg.findall(s) #got a list of the days
+			print "days"
+			print days
+			for iday in range(len(days)):
+				days[iday]=daydict.get(days[iday],False)
+			sentence.append(days)
+			timereg=re.compile(r'\d{1,2}:\d{1,2}\s[a,p].m.')
+			time=timereg.findall(s) #got a list of times
+			print "time"
+			print time
+			if time==[]:
+				time=[0,0]
+			else:
+				start=hrstosecs(time[0])
+				end=hrstosecs(time[1])
+				time=[start,end]
+			print time
+			sentence.append(time)
+			halldeets.append(sentence)
+		allhalls.append(halldeets)
+		print "-------------"
+	return allhalls
 
-for hn in hallnames:
-	resp=str(scrapeapage(True, hn))
-	presp=parseresponse(resp) #List of sentences like: ['Monday - Friday: 11:00 a.m. - 7:00 p.m.', 'Saturday - Sunday: Closed']
-	for s in presp:
-		print s
-		dayreg=re.compile(r'[A-Z,a-z]{3,5}day')
-		days=dayreg.findall(s) #got a list of the days
-		# print "days"
-		# print days
-		timereg=re.compile(r'\d{1,2}:\d{1,2}\s[a,p].m.')
-		time=timereg.findall(s) #got a list of times
-		print "NOTE: NEED TO DEAL WITH THE \"Closed\" TIME"
-		# print "time"
-		# print time
+def tojson(allhalls):
+	"""a function that will take in a list as returned by gethallrawtime() and create a json"""
+	print "to be implemented..."
+
+
+
+
+
+
+
+
+
